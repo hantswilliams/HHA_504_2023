407c74b9e2244e4603acd5d90c003799c68ad4eb
Hants Williams <hantsawilliams@gmail.com>
2023-08-28 13:32:14 -0400

    added search field 2

--- WK1/flask/app.py
+++ WK1/flask/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from functions.data import dataRetrieve
-from functions.search import searchData
+from functions.search import searchCPTcode, searchCPTdescription
 
 app = Flask(__name__)
 
@@ -10,14 +10,32 @@ data = dataRetrieve()
 def index():
     return render_template('index.html', data=data)
 
-@app.route('/find', methods=['GET', 'POST'])
-def find():
+@app.route('/find-code', methods=['GET', 'POST'])
+def find_code_value():
     if request.method == 'POST':
-        search = request.form['search']
-        searchResults = searchData(search)
-        return render_template('index.html', data=searchResults)
+        print('Search term: ', request.form['search'])
+        if request.form['search'] == '':
+            flash('Please enter a search term')
+            return redirect(url_for('index'))
+        else:
+            searchResults = searchCPTcode(request.form['search'])
+            return render_template('index.html', data=searchResults)
     else:
         return redirect(url_for('index'))
+    
+@app.route('/find-code-description', methods=['GET', 'POST'])
+def find_description_value():
+    if request.method == 'POST':
+        print('Search term: ', request.form['search'])
+        if request.form['search'] == '':
+            flash('Please enter a search term')
+            return redirect(url_for('index'))
+        else:
+            searchResults = searchCPTdescription(request.form['search'])
+            return render_template('index.html', data=searchResults)
+    else:
+        return redirect(url_for('index'))
+
     
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
--- WK1/flask/functions/search.py
+++ WK1/flask/functions/search.py
@@ -4,21 +4,17 @@ import sys
 
 sys.path.append('./WK1/flask')
 
-def searchData(search):
+def searchCPTcode(search):
 
-        ## connect to the database and search for CPT code
-        ## if found, return the CPT code, description, setting, type, and price
-        ## if not found, return a message that the CPT code was not found
+        print('Search term: ', search)
 
-        # create a connection to the database
+        # connect to the database
         conn = sqlite3.connect('database.sqlite')
 
         # search term captured
         print('Search term captured: ', search)
 
         cur = conn.cursor()
-
-        # get a random sample of 10 rows from the database
         cur.execute("SELECT * FROM costs_sbm_eastern WHERE cpt = ?", (search,))
 
         # fetch the data
@@ -44,3 +40,40 @@ def searchData(search):
 
         return df
 
+       
+
+def searchCPTdescription(search):
+
+        print('Search term: ', search)
+
+        # connect to the database
+        conn = sqlite3.connect('database.sqlite')
+
+        # search term captured
+        print('Search term captured: ', search)
+
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM costs_sbm_eastern WHERE cpt_description LIKE ?", ('%' + search + '%',))
+
+        # fetch the data
+        data = cur.fetchall()
+
+        # print results
+        print('Returned from search: ', data)
+
+        # save the data to a dataframe
+        df = pd.DataFrame(data)
+
+        # keep only columns 0, 1, 2, 3, 4 in df
+        df = df.iloc[:, 0:5]
+
+        # set column names: 0 is cpt, 1 is cpt_description, 2 is setting, 3 is type, and 4 is price
+        df.columns = ['cpt', 'cpt_description', 'setting', 'type', 'price']
+
+        # print the dataframe
+        print(df)
+
+        # close the connection
+        conn.close()
+
+        return df
